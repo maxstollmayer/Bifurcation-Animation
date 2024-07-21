cdb51df3016d4cde7e4ffe106a1cca7d19169d67
Max <max.stollmayer@gmail.com>
2024-07-21 17:24:42 +0200

    Add quadratic map

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from lib import bifurcate, bifurcate_ode
 import numpy as np
 from numpy.typing import NDArray
+
+from lib import bifurcate, bifurcate_ode
 
 
 def f1(x: NDArray, a: NDArray) -> NDArray:
@@ -23,17 +24,21 @@ def f5(x: NDArray, a: NDArray) -> NDArray:
     return 1 - x - 2 * a * (x - 1) * (2 * x - 1) * x
 
 
+def f6(x: NDArray, a: NDArray) -> NDArray:
+    return x * x + a
+
+
 def main():
-    animation = bifurcate_ode(
-        f1,
-        np.linspace(0.1, 1.1, 1000),
-        np.linspace(0, 4, 4000),
+    animation = bifurcate(
+        f6,
+        np.linspace(-2, 2, 1080),
+        np.linspace(-2, 0.25, 1920),
         max_iter=24 * 10,
-        step_size=0.1,
         alpha=0.01,
     )
-    animation.save("logistic_ode.mp4", dpi=100, fps=24, writer="ffmpeg", bitrate=2400)
+    animation.save("quadratic.mp4", dpi=100, fps=24, writer="ffmpeg", bitrate=2400)
 
 
 if __name__ == "__main__":
     main()
+
